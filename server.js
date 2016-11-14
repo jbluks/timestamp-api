9f7ba67c9546ed331333e619dc13c43b98e9ae8b
Jonathan <jon.bluks@gmail.com>
2016-11-14 23:15:32 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -1,10 +1,48 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+var express = require('express'),
+app = express(),
+port = process.argv[2];
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+if(!port) { 
+  console.error("ERROR: No port specified.");
+  console.error("Usage: node <server.js> <port>");
+  return;
+}
+
+app.get('/', function(req, res) {
+  res.send({"unix": null, "natural": null});
+});
+
+app.get('/:date', function (req, res) {
+  
+  var date = isNaN(Number(req.params.date)) ? 
+    new Date(req.params.date) : 
+    new Date(Number(req.params.date)*1000);
+
+  res.send({ "unix" : date.getTime()/1000, "natural" : formatDateString(date)});
+});
+
+app.listen(port, function () {
+  console.log('Timestamp app listening on port ' + port + '.');
+});
+
+function formatDateString(date) {
+  var months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December"
+  ];
+  
+  return months[date.getMonth()] + " " + date.getDate() + ", " + date.getFullYear();
+}
+
