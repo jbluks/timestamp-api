5cc69fcd0bdfbf33b3f05b906b85e63caff176c8
Jonathan <jon.bluks@gmail.com>
2016-11-16 16:23:50 +0000

    Added process.env.PORT

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var express = require('express'),
+const express = require('express'),
 app = express(),
-port = process.argv[2] || 80;
+port = process.env.PORT || 8080;
 
 
 app.get('/', function(req, res) {
